804371dc22af03fad18085882b7423a00af7c7fb
Anas Oudadsse <anas.oudadsse1@gmail.com>
2024-06-28 22:59:30 +0100

    "Refactored Categories and HeroSection components, updated imports, and modified layout and styling."

--- src/Categories/Categories.jsx
+++ src/Categories/Categories.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
-import { ChakraProvider, Box, Flex, Text, Image, Heading, Button,IconButton } from '@chakra-ui/react';
+import { ChakraProvider, Box, Flex, Text, Image, Heading, Button } from '@chakra-ui/react';
 import { ArrowForwardIcon } from '@chakra-ui/icons';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -9,44 +9,41 @@ import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 const SlickArrowRight = ({ onClick }) => (
   <Box
     onClick={onClick}
-    icon={<FaArrowRight />}
     aria-label="Next slide"
-    variant="ghost"
     position="absolute"
-    right="-150"
+    right={["-10px", "-20px", "-30px"]}
     top="50%"
     transform="translateY(-50%)"
     zIndex="2"
-
+    cursor="pointer"
   >
-    <Image src='rightArrow.svg' alt='right arrow' />
+    <Image src='rightArrow.svg' alt='right arrow' boxSize={["20px", "30px", "40px"]} />
   </Box>
-)
+);
+
 const SlickArrowLeft = ({ onClick }) => (
   <Box
     onClick={onClick}
-    icon={<FaArrowLeft />}
     aria-label="Previous slide"
-    variant="ghost"
     position="absolute"
-    left="-150"
-    top="52.5%"
+    left={["-10px", "-20px", "-30px"]}
+    top="50%"
     transform="translateY(-50%)"
     zIndex="2"
-
-    >
-          <Image src='leftArrow.svg' alt='left arrow' />
-
-    </Box>
+    cursor="pointer"
+  >
+    <Image src='leftArrow.svg' alt='left arrow' boxSize={["20px", "30px", "40px"]} />
+  </Box>
 );
+
 const TrainingCard = ({ image, title, description }) => {
   return (
-    <Box pb={5} textAlign={'start'}  bg="white" borderRadius={20} overflow="hidden" boxShadow="sm" m={4} maxW="sm">
+    <Box pb={5} textAlign={'start'} bg="white" borderRadius={20} overflow="hidden" boxShadow="sm" m={4} maxW="sm">
       <Image src={image} alt={title} objectFit="fit" />
       <Box p={4}>
         <Heading as="h4" size="md" mb={2}>{title}</Heading>
         <Text fontWeight={'regular'} mb={4}>{description}</Text>
-        <Button mt={5} transform={'scale(0.9)'}  rightIcon={<ArrowForwardIcon />} colorScheme="black" variant="outline">
+        <Button mt={5} transform={'scale(0.9)'} rightIcon={<ArrowForwardIcon />} colorScheme="black" variant="outline">
           Sport Information
         </Button>
       </Box>
@@ -57,17 +54,17 @@ const TrainingCard = ({ image, title, description }) => {
 const TrainingsSection = () => {
   const trainings = [
     {
-      image: 'weightlifting.svg', 
+      image: 'weightlifting.svg',
       title: 'Weight Lifting',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     },
     {
-      image: 'weightloss.svg', 
+      image: 'weightloss.svg',
       title: 'Weight Loss',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     },
     {
-      image: 'Yoga.svg', 
+      image: 'Yoga.svg',
       title: 'Yoga',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
     },
@@ -77,19 +74,31 @@ const TrainingsSection = () => {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 3 ,
+    slidesToShow: 3,
     slidesToScroll: 1,
-    nextArrow: null,
     nextArrow: <SlickArrowRight />,
     prevArrow: <SlickArrowLeft />,
     adaptiveHeight: true,
-  
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
     <ChakraProvider>
-      <Flex bg={'#f4f4f4'} justify="center" align="center"  p={20} minHeight="100vh">
-        <Box width={["100%", "90%", "80%"]} textAlign="center" p={5} >
+      <Flex bg={'#f4f4f4'} justify="center" align="center" p={[10, 20]} minHeight="100vh">
+        <Box width={["100%", "90%", "80%"]} textAlign="center" p={5}>
           <Heading pb={5} as="h2" size="xl" mb={8}>
             Discover All our Trainings
           </Heading>
@@ -105,7 +114,6 @@ const TrainingsSection = () => {
           </Slider>
         </Box>
       </Flex>
-
     </ChakraProvider>
   );
 };
--- src/HeroSection/HeroSection.jsx
+++ src/HeroSection/HeroSection.jsx
@@ -2,62 +2,67 @@ import { Box, Button, Flex, Heading, Icon, Text } from "@chakra-ui/react";
 import React from "react";
 import { FaLinkedin, FaInstagram, FaFacebook } from 'react-icons/fa';
 
-
 export const HeroSection = () => {
     return (
-
-        <Flex                 
-        w={["150%", "120%", "100%"]}
-      
-        mb={[10, 10, 0]} bg={'white'}>
-
-        {/* Text */}
-        <Box m={5} transform={'scale(1.1)'} w={500}  pos={'relative'} top={190} left={210}>
-
-            <Box    transform={'scale(1.3)'}>
-
-                <Heading fontWeight={'Bold'} fontFamily={'inter'} color={'#2d2d2d'}>
-                FIND YOUR PERFECT PERSONAL COACH, ANYTYME, ANYWHERE
-                </Heading>
-
-                <Text fontSize={15} mt={3} w={460} color={'gray.500'} fontWeight={'regular'}>
-                Connecting you with certified coaches and personal trainers for tailored fitness journeys
-                </Text>
-
-            </Box>
-
-            <Flex mt={20} pos={'relative'} right={70} >
-
-                <Button _hover={{bg:"gray.600"}} h={42} w={150} mr={2}  bg={'#2d2d2d'} color={'white'} borderRadius={0}>
-                    Get Started
-                </Button>
-
-                <Button fontFamily={'inter'} w={150} borderRadius={0} bg={'transparent'} color={'black'} fontWeight={'bold'}>
-
-                    <img style={{marginRight:12, width:30}} src="play-circle.svg" alt="play" />
-
-                            Play video
-                </Button>
-
-            </Flex>
-
-            <Box color={'black'} display="flex" justifyContent="space-around" alignItems="center" w="200px" p="4" borderRadius="md" pos={'relative'} right={101} mt={5}>
-                    <Icon href="#" as={FaLinkedin} w={6} h={6} color="#2d2d2d" />
-                    <Icon as={FaInstagram} w={6} h={6} color="#2d2d2d" />
+        <Flex
+            w={["100%", "100%", "100%"]}
+            mb={[10, 10, 0]}
+            bg={'white'}
+            direction={['column', 'column', 'row']}
+            alignItems="center"
+            justifyContent="center"
+            p={[5, 5, 10]}
+        >
+            {/* Text */}
+            <Box
+                m={5}
+                w={["100%", "80%", "50%"]}
+                textAlign={["center", "center", "left"]}
+                order={[2, 2, 1]}
+            >
+                <Box>
+                    <Heading fontWeight={'Bold'} fontFamily={'inter'} color={'#2d2d2d'}>
+                        FIND YOUR PERFECT PERSONAL COACH, ANYTYME, ANYWHERE
+                    </Heading>
+                    <Text fontSize={["sm", "md", "lg"]} mt={3} color={'gray.500'} fontWeight={'regular'}>
+                        Connecting you with certified coaches and personal trainers for tailored fitness journeys
+                    </Text>
+                </Box>
+                <Flex mt={5} justifyContent={["center", "center", "flex-start"]}>
+                    <Button
+                        _hover={{ bg: "gray.600" }}
+                        h={42}
+                        w={150}
+                        mr={2}
+                        bg={'#2d2d2d'}
+                        color={'white'}
+                        borderRadius={0}
+                    >
+                        Get Started
+                    </Button>
+                    <Button
+                        fontFamily={'inter'}
+                        w={150}
+                        borderRadius={0}
+                        bg={'transparent'}
+                        color={'black'}
+                        fontWeight={'bold'}
+                    >
+                        <img style={{ marginRight: 12, width: 30 }} src="play-circle.svg" alt="play" />
+                        Play video
+                    </Button>
+                </Flex>
+                <Flex mt={5} justifyContent={["center", "center", "flex-start"]}>
+                    <Icon as={FaLinkedin} w={6} h={6} color="#2d2d2d" mr={4} />
+                    <Icon as={FaInstagram} w={6} h={6} color="#2d2d2d" mr={4} />
                     <Icon as={FaFacebook} w={6} h={6} color="#2d2d2d" />
-            </Box>
-        </Box>
-
-
-        {/* Illustration */}
-
-            <Box w={"30%"}  position={'relative'} left={360} bottom={3.5} >
-
-                <img  src="coach.png" alt="coaches" />
-
+                </Flex>
             </Box>
 
+            {/* Illustration */}
+            <Box w={["100%", "80%", "50%"]} order={[1, 1, 2]} mb={[5, 5, 0]}>
+                <img src="coach.png" alt="coaches" width="100%" />
+            </Box>
         </Flex>
-
-    )
+    );
 }
